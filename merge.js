258066960c9393a68d6d92a8df64a38669e7dd84
Akhaylin Rajagopaul <akhaylin.r@gmail.com>
2024-01-05 17:10:56 -0500

    finished mergeSort

--- merge.js
+++ merge.js
@@ -1,5 +1,40 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let merged = [];
+  let i = 0;
+  let j = 0;
 
-function mergeSort() {}
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
 
-module.exports = { merge, mergeSort};
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+
+  return merged;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1){
+    return arr;
+  }
+  debugger;
+  let middle = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, middle));
+  let right = mergeSort(arr.slice(middle));
+
+  return merge(left, right)
+ }
+
+module.exports = { merge, mergeSort };
